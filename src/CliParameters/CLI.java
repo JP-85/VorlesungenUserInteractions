69bb886dda12fddd366ce25b64ec44b7ed3f7fc7
Jan Popko <jan.popko@gmx.de>
2026-01-07 13:15:21 +0100

    UPDATE exercise 1 semi finished

--- src/CliParameters/CLI.java
+++ src/CliParameters/CLI.java
@@ -1,8 +1,19 @@
 package CliParameters;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CLI {
-    public static void main(String[] args) {
-        String filename = args[0];
-        System.out.println(filename);
+    public static void main(String[] args) throws IOException {
+//        String filename = args[0];
+//        System.out.println(filename);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter your name:");
+        String name = br.readLine();
+        System.out.println("Welcome " + name);
+
     }
 }
